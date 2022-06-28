97603d0ad09a9173d8edcdb4b2c9bd6116f7dd2f
Roman Rudenko <ruroma18@gmail.com>
2022-06-28 16:26:13 +0300

    Add User controller and middleware

---
+++ controllers/userController.js
@@ -0,0 +1,76 @@
+const { Users } = require('../models');
+const createError = require('http-errors');
+
+module.exports.createUser = async (req, res, next) => {
+  try {
+    const { body } = req;
+
+    const user = await Users.create(body);
+
+    if (!user) {
+      return next(createError(400, 'Cant create user'));
+    }
+
+    res.send({ data: user })
+
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.findUsers = async (req, res, next) => {
+  try {
+
+    const users = await Users.findAll({
+      attributes: {
+        exclude: ['password']
+      }
+    })
+
+    res.send({ data: users });
+
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.findUserById = async (req, res, next) => {
+  try {
+
+    const { user } = req;
+
+    res.send({ data: user });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.updateUser = async (req, res, next) => {
+  try {
+    const { body, user } = req;
+
+    const updatedUser = await user.update(body, {
+      returning: true
+    })
+
+    res.send({ data: updatedUser });
+
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.deleteUser = async (req, res, next) => {
+  try {
+
+    const { user } = req;
+
+    await user.destroy();
+
+    res.send({ data: user });
+
+  } catch (error) {
+    next(error);
+  }
+};
+
---
+++ middlewares/userMW.js
@@ -0,0 +1,25 @@
+const { Users } = require('../models');
+const createError = require('http-errors');
+
+module.exports.findUser = async (req, res, next) => {
+  try {
+    const { params: { userId } } = req;
+
+    const user = await Users.findByPk(userId, {
+      attributes: {
+        exclude: ['password']
+      }
+    });
+
+    if (!user) {
+      return next(createError(404, 'User not found'))
+    }
+
+    req.user = user;
+
+    next();
+
+  } catch (error) {
+    next(error);
+  }
+}
